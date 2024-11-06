7a2af9f1ceb8dfcdae0edb4e6c247ff1d1d5dd36
sbfrusho <rushocseru28@gmail.com>
2024-11-06 09:15:23 +0600

    1-4 done

--- signal_generation.py
+++ signal_generation.py
@@ -10,27 +10,24 @@ def ramp_func(n):
 def exponential_func(n, a):
     return np.exp(a * n)
 
-def sin_signal(f1, fs, t):
-    return np.sin(2 * np.pi * (f1/fs) * t)
+def sin_signal(f1, t):
+    return np.sin(2 * np.pi * f1 * t)
 
-def cos_signal(f1, fs, t):
-    return np.cos(2 * np.pi * f1 * t / fs)
+def cos_signal(f1, t):
+    return np.cos(2 * np.pi * f1 * t)
 
-f1 = 5       
-fs = 20      
-n = np.arange(-10, 10, 1)   
-  
+f1 = 5
+fs = 20
+n = np.arange(-10, 10, 1)
+t = np.linspace(-1, 1, 1000)
 
 unit_signal = unit_step(n)
 ramp_signal = ramp_func(n)
 exponential_signal = exponential_func(n, 0.2)
 
-t = np.linspace(-1, 1, 1000)
-continuous_signal = sin_signal(f1, 1, t)     
-
-nts = np.arange(-1, 1, 1/fs)                 
-discrete_signal = sin_signal(f1, fs, nts)     
-
+continuous_signal = sin_signal(f1, t)
+nts = np.arange(-1, 1, 1/fs)
+discrete_signal = sin_signal(f1, nts)
 
 plt.figure(figsize=(16, 10))
 
@@ -54,7 +51,7 @@ plt.ylabel("Amplitude")
 
 plt.subplot(3, 2, 4)
 plt.plot(t, continuous_signal, label="Continuous Sine", color="blue")
-plt.plot(nts, discrete_signal)
+plt.stem(nts, discrete_signal, linefmt="red", markerfmt="o", basefmt="black")
 plt.title("Continuous Sine Signal")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
@@ -69,11 +66,11 @@ plt.ylabel("Amplitude")
 plt.grid(True)
 
 fs = 8
-nts = np.arange(-1, 1, 1/fs)                 
-discrete_signal = sin_signal(f1, fs, nts)
+nts = np.arange(-1, 1, 1/fs)
+discrete_signal_alias = sin_signal(f1,nts)
 plt.subplot(3, 2, 6)
 plt.plot(t, continuous_signal, label="Continuous Sine", color="blue")
-plt.plot(nts, discrete_signal)
+plt.stem(nts, discrete_signal_alias, linefmt="red", markerfmt="o", basefmt="black")
 plt.title("Alias Sine Signal")
 plt.xlabel("Time")
 plt.ylabel("Amplitude")
